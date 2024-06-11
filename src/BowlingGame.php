3787ec93acfa8a088baf056a2cc8c6e424e8aee9
SadiqSaira <sairaishaq@gmail.com>
2024-06-12 00:10:41 +0100

    test4: Added unit test for spare frame scenario
    This commit includes unit tests to verify the behavior of the game when the player knocks down all 10 pins in two rolls.

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -1,8 +1,29 @@
 <?php 
 class BowlingGame{
     private $totalScore = 0;
+    private $isSpare = false;
+    private $rollOneOrTwo = 1;
+    private $totalFrameScore = 0;
     public function roll(int $knockeDownPins){
-        $this->totalScore += $knockeDownPins;
+        if($this->isSpare){
+            $this->totalScore +=$knockeDownPins;
+            $this->isSpare = false;
+        }
+        if($this->rollOneOrTwo == 1){
+            $this->rollOneOrTwo = 2;
+            $this->totalFrameScore += $knockeDownPins; 
+
+        }else if($this->rollOneOrTwo == 2){
+            $this->rollOneOrTwo = 1;
+            $this->totalFrameScore += $knockeDownPins; 
+            $this->totalScore += $this->totalFrameScore;
+
+            if($this->totalFrameScore == 10){
+                $this->isSpare = true;
+            }
+            $this->totalFrameScore = 0;
+        }
+
     }
     public function score():int{
         return $this->totalScore;
--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -39,6 +39,25 @@ final class BowlingGameTest extends TestCase
     public function test_recording_total_score_of_ten_frame_gutter_game(): void
     {
         $allScores = [
+            2, 2,
+            5, 5,
+            2, 4,
+            7, 3,
+            3, 4,
+            1, 4,
+            6, 2,
+            4, 4,
+            1, 3,
+            0, 5
+            ];
+        foreach ($allScores as $score) {
+            $this->bowlingGame->roll($score);
+        }
+        $this->assertEquals(72, $this->bowlingGame->score());
+    }
+    public function test_add_bonus_in_total_score_when_spare_frame(): void
+    {
+        $allScores = [
             0, 0,
             0, 0,
             0, 0,
