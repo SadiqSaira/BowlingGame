c2f014583023e14ffca4a791f2df1f8318c4240d
SadiqSaira <sairaishaq@gmail.com>
2024-06-12 08:56:47 +0100

    refactor: Refactored  BowlingGame class

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -7,6 +7,22 @@ class BowlingGame{
     private $totalFrameScore = 0;
     public function roll(int $knockeDownPins){
         // Processing bonus roll(s) for a spare and a strike in the last frame
+        $this->processingBonusBall($knockeDownPins);
+
+        //if first roll add it in frame total 
+        if($this->rollOneOrTwo == 1){
+            $this->handleFirstBall($knockeDownPins);
+
+        // if second roll add it in frame total and in grand total
+        }else {
+            $this->handleSecondBall($knockeDownPins);
+        }
+        
+    }
+    public function score():int{
+        return $this->totalScore;
+    }
+    private function processingBonusBall(int $knockeDownPins){
         if($this->isSpare){
             $this->totalScore +=$knockeDownPins;
             
@@ -18,29 +34,24 @@ class BowlingGame{
                 $this->isSpare = false;
             }
         }
-        //if first roll add it in frame total 
-        if($this->rollOneOrTwo == 1){
-            $this->rollOneOrTwo = 2;
-            $this->totalFrameScore += $knockeDownPins; 
-        
-        // if second roll add it in frame total and in grand total
-        }else if($this->rollOneOrTwo == 2){
-            $this->rollOneOrTwo = 1;
-            $this->totalFrameScore += $knockeDownPins; 
-            $this->totalScore += $this->totalFrameScore;
-            
-            //if frame total is 10 or more set the spare flag
-            if($this->totalFrameScore >= 10){
-                $this->isSpare = true;
-            }
-            $this->totalFrameScore = 0;
-        }
-        //if score of first roll is 10,  set the strike flag 
-        if($knockeDownPins == 10){
-            $this->strike = true;
-        }
     }
-    public function score():int{
-        return $this->totalScore;
+    private function handleFirstBall(int $knockeDownPins){
+        $this->rollOneOrTwo = 2;
+            $this->totalFrameScore += $knockeDownPins; 
+            //if score of first roll is 10,  set the strike flag 
+            if($knockeDownPins == 10){
+                $this->strike = true;
+            }
+    }
+    private function handleSecondBall(int $knockeDownPins){
+        $this->rollOneOrTwo = 1;
+        $this->totalFrameScore += $knockeDownPins; 
+        $this->totalScore += $this->totalFrameScore;
+
+        //if frame total is 10 or more set the spare flag
+        if($this->totalFrameScore >= 10){
+            $this->isSpare = true;
+        }
+        $this->totalFrameScore = 0;
     }
 }
